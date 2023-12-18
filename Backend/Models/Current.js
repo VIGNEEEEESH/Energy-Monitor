89976de221973ad0c1c17d3355320a9e67b3a7ab
Vignesh Devamulla <103365363+VIGNEEEEESH@users.noreply.github.com>
2023-12-18 17:09:37 +0530

    commit

--- Backend/Models/Current.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const currentSchema = new Schema({
-  sensor: { type: String, required: true },
-  dateandtime: { type: String, required: true },
-  energy: { type: String, required: true },
-  voltage: { type: String, required: true },
-});
-module.exports = mongoose.model("Current", currentSchema);
